377dfeac96f8dac53dcbeb7787d537de4a18cc03
Nico De Witte <nico.dewitte@vives.be>
2020-10-31 09:09:45 +0100

    Add code

---
+++ ForLoopChallengesGroup2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ForLoopChallengesGroup2
+{
+    class Program
+    {
+        // 3, 6, 9, 12, ...
+        // count = number of multiples
+        // baseNumber = multiple of
+        public static void Multiples(
+            int count, int baseNumber)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine((i + 1) * baseNumber);
+            }
+        }
+
+        // Counting uppercase characters in string
+        public static int CountUpperCaseLetters
+            (string text)
+        {
+            int upperCase = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                char letter = text[i];
+                if (letter >= 'A' && letter <= 'Z')
+                {
+                    upperCase++;
+                }
+            }
+            return upperCase;
+        }
+
+        public static int ConvertToNumber(string value)
+        {
+            int multiplier = 1;     // 1 (10^0), 10 (10^1), 100 (10^2) ...
+            int sum = 0;
+            for (int i = value.Length-1; i >= 0; i--)
+            {
+                char digit = value[i];
+                int digitValue = (digit - '0') * multiplier;
+
+                sum += digitValue;
+
+                multiplier = multiplier * 10;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Multiples(10, 7);
+
+            string text = "The Quick Brown Fox Jumps over the Lazy Dog";
+            Console.WriteLine(
+                $"{text} contains {CountUpperCaseLetters(text)} uppercase letters"
+            );
+
+            Console.WriteLine(ConvertToNumber("432343")+1);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Class Demo - For Loop Challenges - Group 2
+
+Some different algorithms using for-loop constructs.
+
+## How to get this locally:
+
+Go to a local directory on your system and open Powershell in that directory. Now clone this repo using git:
+
+```bash
+git clone git@github.com:vives-introduction-to-programming-2020/ForLoopChallengesGroup2.git
+```
